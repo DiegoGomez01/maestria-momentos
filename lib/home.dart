6261c92e2090a2447cfec62d0725fb893c5bb6aa
Diego F. Gómez <gomezdiego1998@gmail.com>
2019-11-15 21:22:03 -0500

    add markers in map

--- lib/home.dart
+++ lib/home.dart
@@ -4,6 +4,7 @@ import 'package:latlong/latlong.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
+import 'dart:async';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -16,19 +17,64 @@ class Home extends StatefulWidget {
 
 class _HomePageState extends State<Home> {
 
-  getPermission() async {
-    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    //double distanceInMeters = await Geolocator().distanceBetween(52.2165157, 6.9437819, 52.3546274, 4.8285838);
-    print(position);
+  Position _currentPosition;
+  var _markersView = [];
+
+  addMarker(marker) async {
+    double distanceInMeters = await Geolocator().distanceBetween(
+        _currentPosition.latitude, _currentPosition.longitude, marker['latitude'], marker['longitude']);
+    if(distanceInMeters < 1000) {
+      return new Marker(
+        width: 45.0,
+        height: 45.0,
+        point: new LatLng(marker['latitude'], marker['longitude']),
+        builder: (context) => new Container(
+          child: IconButton(
+            icon: Icon(Icons.location_on),
+            color: Colors.blueAccent,
+            iconSize: 45.0,
+            onPressed: () {
+              print(marker['title']);
+            },
+          ),
+        )
+      );
+    }
+    return null;
   }
 
-  Position _currentPosition;
+  getMarkers() async {
+    var markers = [
+      new Marker(
+          width: 45.0,
+          height: 45.0,
+          point: new LatLng(_currentPosition.latitude, _currentPosition.longitude),
+          builder: (context) => new Container(
+            child: IconButton(
+              icon: Icon(Icons.location_on),
+              color: Colors.redAccent,
+              iconSize: 45.0,
+              onPressed: () {
+                print('Marker tapped');
+              },
+            ),
+          )
+      )
+    ];
+    var marks = await _getAllPublications();
+    for (var i = 0; i<marks['data'].length; i++) {
+      var marker = await addMarker(marks['data'][i]);
+      if(marker != null) {
+        markers.add(marker);
+      }
+    }
+    return markers;
+  }
 
   @override
   Widget build(BuildContext context) {
     _getCurrentLocation();
-    //_getAllPublications();
-    getdata();
+    print(_markersView);
     return Scaffold(
       appBar: AppBar(
         title: Text("Location"),
@@ -44,70 +90,55 @@ class _HomePageState extends State<Home> {
                   'pk.eyJ1IjoiZGllZ29nb21leiIsImEiOiJjam5qaW1xeXMwenNsM2tueXFiZWZ1andiIn0.suJs58a1pOpL4c-rQzU2VA',
                   'id': 'mapbox.mapbox-streets-v7'
                 }),
-            new MarkerLayerOptions(markers: [
-              new Marker(
-                  width: 45.0,
-                  height: 45.0,
-                  point: new LatLng(_currentPosition.latitude, _currentPosition.longitude),
-                  builder: (context) => new Container(
-                    child: IconButton(
-                      icon: Icon(Icons.location_on),
-                      color: Colors.red,
-                      iconSize: 45.0,
-                      onPressed: () {
-                        print('Your position');
-                      },
-                    ),
-                  )
-              )
-            ])
+            new MarkerLayerOptions(markers: _markersView)
           ]
       )
     );
   }
 
-  _getCurrentLocation() {
-    final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
+  _getCurrentLocation() async {
+    var geolocator = Geolocator();
+    var locationOptions = LocationOptions(accuracy: LocationAccuracy.high, distanceFilter: 10);
+    StreamSubscription<Position> positionStream = geolocator.getPositionStream(locationOptions).listen(
+      (Position position) {
+        if(position != 'Unknown') {
+          setState(() {
+            _currentPosition = position;
+            getMarkers().then((marks) => {
+              setState(() {
+                _markersView = marks;
+              }
+            )});
+          });
+        }
+    });
+  }
 
+  _getCurrentLocation2() {
+    final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
     geolocator
         .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
         .then((Position position) {
       setState(() {
         _currentPosition = position;
       });
-    }).catchError((e) {
-      // print(e);
     });
   }
 
-  Future getdata() async {
-    var res = await http.get(
-        Uri.encodeFull("http://10.0.2.2:3000/publications/range/20"),
-        headers: {
-          "Accept": "application/json",
-          "Content-Type": "application/json",
-          "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwibmFtZSI6IkRpZWdvIiwiZW1haWwiOiJnb21lemRpZWdvMTk5OEBnbWFpbC5jb20iLCJpYXQiOjE1NzMzMTIxNjksImV4cCI6MTU3MzM5ODU2OX0.N1JA4EIAlXftpefO6U-RfN2CABXmbE7TdnbEWs3OEDQ"
-        }
-    );
-    print(res.body);
-  }
-
   _getAllPublications() async {
-    print('llego');
     var response = await http.get(
-        Uri.encodeFull("http://10.0.2.2:3000"),
+        Uri.encodeFull("http://192.168.1.63:3000/publications"),
         headers: {
           "Accept": "application/json",
           "Content-Type": "application/json",
-          "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwibmFtZSI6IkRpZWdvIiwiZW1haWwiOiJnb21lemRpZWdvMTk5OEBnbWFpbC5jb20iLCJpYXQiOjE1NzMzMTIxNjksImV4cCI6MTU3MzM5ODU2OX0.N1JA4EIAlXftpefO6U-RfN2CABXmbE7TdnbEWs3OEDQ"
+          "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwibmFtZSI6IkRpZWdvIiwiZW1haWwiOiJnb21lemRpZWdvMTk5OEBnbWFpbC5jb20iLCJpYXQiOjE1NzM4Njg4MTgsImV4cCI6MTU3Mzk1NTIxOH0.SrWAYxhSjqbHkXfj_W8xNCNwNN9Q-pkrKTuV6jFxlK8"
         }
     );
-    if (response.statusCode > 200 && response.statusCode < 210) {
-      var jsonResponse = convert.jsonDecode(response.body);
-      //var itemCount = jsonResponse['totalItems'];
-      print("Number of books about http: $jsonResponse.");
+    if (response.statusCode >= 200 && response.statusCode < 210) {
+      return convert.jsonDecode(response.body);
     } else {
-      print("Request failed with status: ${response.statusCode}.");
+      print(response.statusCode);
+      return convert.jsonDecode("data:[]");
     }
   }
 }
